cfd05f571384a00a67526acf1cf76683bddacc53
pacorbemo <pacorbemo@gmail.com>
2025-06-13 17:34:10 +0200

    https

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
-const https = require("http");
+const https = require("https");
 const app = require("./app");
 const socketHandler = require('./socket');
 const { db } = require('./db/db.config');
 const PORT = 5000;
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/corb3ra.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/corb3ra.com/fullchain.pem')
+};
 
-const server = https.createServer(app);
+const server = https.createServer(options, app);
 socketHandler(server, db);
 
 if (require.main === module) {
@@ -12,7 +17,7 @@ if (require.main === module) {
     const address = server.address();
     let host = address.address;
     if (host === '::' || host === '0.0.0.0') host = 'localhost';
-    console.log(`Servidor corriendo en http://${host}:${address.port}`);
+    console.log(`Servidor corriendo en https://${host}:${address.port}`);
   });
 }
 
